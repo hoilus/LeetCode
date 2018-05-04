31ec54d01f736b9cf0dd43c594aee64dc7e4fede
Liang Hong <hoilus@users.noreply.github.com>
2018-05-04 00:16:59 -0500

    Create 030-Substring with Concatenation of All Words.py

---
+++ 030-Substring with Concatenation of All Words.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        res = []
+        if (len(words) == 0) or (len(s) == 0):
+            return res
+        lenwords = len(words)
+        len1word = len(words[0])
+        totletters = len1word*lenwords
+        for i in range(len(s)-totletters+1):
+            tmpwords = words[:]
+            for j in range(lenwords):
+                tmps = s[(i+j*len1word):(i+(j+1)*len1word)]
+                if tmps in tmpwords:
+                    tmpwords.remove(tmps)
+                else:
+                    break
+            if len(tmpwords) == 0:
+                res.append(i)
+        return res
