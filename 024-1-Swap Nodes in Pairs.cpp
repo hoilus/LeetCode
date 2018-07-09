83330bb2ec720fd60ec55df48ecc2cf48cdc38d0
Liang Hong <hoilus@users.noreply.github.com>
2018-07-08 21:49:12 -0500

    runtime 0ms, cpp

---
+++ 024-1-Swap Nodes in Pairs.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode **p = &head, *a, *b;
+        while ((a = *p) && (b = a->next)) {
+            a->next = b->next;
+            b->next = a;
+            *p = b;
+            p = &(a->next);
+        }
+        return head;
+    }
+};
